6301419ae1d4f54f593d96c01db729187b57d1e0
Jeff Uhrick <jeff.uhrick@kikoda.com>
2021-08-15 02:00:04 -0400

    Adding scaffolding for Translator

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 // import Accordion from './components/Accordion';
 // import Search from './components/Search';
 import Dropdown from './components/Dropdown';
-
+import Translate from './components/Translate';
 // const items = [
 //     {
 //         title: 'What is React?',
@@ -33,25 +33,10 @@ const options = [
 ];
 
 const App = () => {
-    const [selected, setSelected] = useState(options[0]);
-    const [showDropdown, setShowDropdown] = useState(true);
 
     return (
         <div>
-            {/* <Accordion items={ items }/> */}
-            {/* <Search /> */}
-            <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-            {
-                showDropdown ?
-                    <div>
-                        <Dropdown
-                            selected={selected}
-                            onSelectedChange={setSelected}
-                            options={options}
-                        />
-                        <span style={{color: selected.value}}>The color is {selected.label} </span>
-                    </div> : null
-            }
+            <Translate />
         </div>
     )
 };
